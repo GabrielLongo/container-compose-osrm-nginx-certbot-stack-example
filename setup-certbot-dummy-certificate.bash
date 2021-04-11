b26b2c105df9ebfc005626572abda7bd661db12c
Gabriel Longo <gabriel.longo.gabriel@gmail.com>
2021-04-11 14:43:32 -0300

    Fix problematic coding in shell (Shellcheck SC2128, SC2145)

--- setup-certbot-dummy-certificate.bash
+++ setup-certbot-dummy-certificate.bash
@@ -6,6 +6,7 @@ if ! [ -x "$(command -v podman-compose)" ]; then
   exit 1
 fi
 
+# The domain order matters, domain go first, sub-domain go next.
 domains=(example.org www.example.org)
 rsa_key_size=4096
 data_path="./container-stack-data/certbot/"
@@ -13,7 +14,7 @@ email="" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+  read -p "Existing data found for ${domains[*]}. Continue and replace existing certificate? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
     exit
   fi
@@ -28,9 +29,9 @@ if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/
   echo
 fi
 
-echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
+echo "### Creating dummy certificate for ${domains[0]} ..."
+path="/etc/letsencrypt/live/${domains[0]}"
+mkdir -p "$data_path/conf/live/${domains[0]}"
 podman-compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
@@ -43,15 +44,15 @@ echo "### Starting nginx ..."
 podman-compose up --force-recreate -d nginx
 echo
 
-echo "### Deleting dummy certificate for $domains ..."
+echo "### Deleting dummy certificate for ${domains[0]} ..."
 podman-compose run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+  rm -Rf /etc/letsencrypt/live/${domains[0]} && \
+  rm -Rf /etc/letsencrypt/archive/${domains[0]} && \
+  rm -Rf /etc/letsencrypt/renewal/${domains[0]}.conf" certbot
 echo
 
 
-echo "### Requesting Let's Encrypt certificate for $domains ..."
+echo "### Requesting Let's Encrypt certificate for ${domains[*]} ..."
 #Join $domains to -d args
 domain_args=""
 for domain in "${domains[@]}"; do
